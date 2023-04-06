d1a349cc51943360fdb24a40b805555b041c0977
kishore <kishorekumar19csevec@gmail.com>
2023-04-06 13:47:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn =  document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+//random quote function
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading quote....";
+
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New quote";
+        quoteBtn.classList.remove("loading");
+
+
+    });
+}
+soundBtn.addEventListener("click", ()=>{
+    //the speechsynthesisUtterance is a web speech api that represents a speech request
+
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+    
+});
+
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl,"_blank");
+    
+    
+});
+
+quoteBtn.addEventListener("click",randomQuote);
